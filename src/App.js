371e774baa2cb420d26812165aea38c98e040adc
Agustín Vargas <agustinvargas93@gmail.com>
2022-04-22 01:05:27 -0300

    add footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Board from './components/Board/Board';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <Header />
       <Board />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className='footer'>
+        <small>
+          Developed by{' '}
+          <a
+            target='_blank'
+            href='https://github.com/agustinvargas'
+            rel='noreferrer'
+          >
+            @agustinvargas
+          </a>
+        </small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
